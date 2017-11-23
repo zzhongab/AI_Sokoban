1d6f09ed0c58e87f8eaefb3ba22ec42403a7dca3
刘殊玥 <sliuao@liushuyuedeMacBook-Pro.local>
2017-11-23 19:56:23 +0800

    State.java javadoc

--- src/StarOG.java
+++ src/StarOG.java
@@ -1,15 +1,20 @@
 import java.util.Comparator;
 
-/**
- * 
- */
+
 
 /**
- * @author wgf2104
- *
+ * A class to compare two State
+ * @author COMP 3211 Group6
+ * @since 2017-11-23
  */
 public class StarOG implements Comparator<State> {
 
+	/**
+	 * Compare two States' sum of opengoal and cost
+	 * @param o1 input State1
+	 * @param o2 input State2
+	 * @return result return an integer to give the result of comparision
+	 */
 	@Override
 	public int compare(State o1, State o2) {
 		if((o1.openGoals() + o1.getCost()) == (o2.openGoals() + o2.getCost()))
