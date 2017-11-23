98724a54705d50308ba10d541b4710d20ec06f27
sliuao <31642479+sliuao@users.noreply.github.com>
2017-11-23 20:11:27 +0800

    javadoc added

--- src/ManhDist.java
+++ src/ManhDist.java
@@ -1,15 +1,19 @@
 import java.util.Comparator;
 
-/**
- * 
- */
 
 /**
- * @author wgf2104
- *
+ * This class is to compare the manhanttan distance between two state
+ * @author COMP 3211 Group
+ * @since 2017-11-23
  */
 public class ManhDist implements Comparator<State> {
 
+	/**
+	 * compare two states' manhanttan distance
+	 * @param o1 input State1
+	 * @param o2 input State2
+	 * @return result an integer to show the result of comparision
+	 */
 	@Override
 	public int compare(State o1, State o2) {
 		if(o1.manhDist() == o2.manhDist())
