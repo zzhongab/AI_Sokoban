8d3567c657e4b0519d097c71905b431ad195cfb4
gwangaj <gwangaj@connect.ust.hk>
2017-11-23 20:35:15 +0800

    Q doc

--- src/reinforcement_learning/QLearning.java
+++ src/reinforcement_learning/QLearning.java
@@ -3,24 +3,91 @@ package reinforcement_learning;
 import java.io.File;
 //import java.util.Scanner;
 import java.util.*;
-
+/**
+ * This class is a container for QLearning object which is the main part of reinforcement learning
+ * @author COMP3211 Group 6
+ */
 public class QLearning {
+	/**
+	 * This is the learning rate for Q learning
+	 */
 	private final double alpha = 0.3; // Learning rate
+	
+	/**
+	 * This is the reward decay for Q learning
+	 */
 	private final double gamma = 0.9; // Eagerness - 0 looks in the near future, 1 looks in the distant future
-	private final double reward = 1000; // one of box reaches a goal
+	
+	/**
+	 * This is the reward when one of boxes reaches a goal
+	 */
+	private final double reward = 1000; // one of boxes reaches a goal
+	
+	/**
+	 * This is the decay parameter for randomization
+	 */
 	private final double decay = 0.999; // decay for randomization
+	
+	/**
+	 * This is the parameter for epsilon greedy
+	 */
 	private final int k = 100; // parameter for epsilon greedy
-	private final double penalty = 0; // one of box reaches a deadlock
+	
+	/**
+	 * This is the penalty when one of boxes reaches a deadlock
+	 */
+	private final double penalty = 0; // one of boxes reaches a deadlock
+	
+	/**
+	 * This is the reward when the player pushes a box
+	 */
 	private final double push_reward = 0;// encourage the player to push box
+	
+	/**
+	 * This is the living penalty
+	 */
 	private final double timePenalty = 0;// living penalty for training in order to optimize move steps
-	private final int trainingEpoch = 10000;// num of epoches to be trained
+	
+	/**
+	 * This is the number of training epochs
+	 */
+	private final int trainingEpoch = 10000;// num of epochs to be trained
+	
+	/**
+	 * This is the parameter for exploration function
+	 */
 	private final double trialTimesParam = 0; // parameter for exploration function
+	
+	/**
+	 * This is the number of steps to move for print strategy
+	 */
 	private final int numSteps = 100; // num of steps to move;
+	
+	/**
+	 * This is the Q table
+	 */
 	private HashMap<QNode, Double> table; // Q table
+	
+	/**
+	 * This is the variable to record number of success
+	 */
 	private int numOfSuccess = 0;
+	
+	/**
+	 * This is the initial state for Q learning
+	 */
 	private State initState;
+	
+	/**
+	 * This is the current state for Q learning
+	 */
 	private State state;
-
+    
+	/**
+     * This method is used to execute Q learning
+     * @param  filePath the file to be loaded
+     * @return strategy for Q learning
+     */
 	public static String execute(String filePath) {
 		QLearning ql = new QLearning();
 
@@ -32,6 +99,10 @@ public class QLearning {
 		return ql.getStrategyFromTable();
 		}
 
+	/**
+     * This method is to do initialization for Q learning
+     * @param  filePath the file to be loaded
+     */
 	public void init(String filePath) {
 		File lvl_src = new File(filePath);
 		LevelLoader ll = new LevelLoader(lvl_src);
@@ -42,6 +113,9 @@ public class QLearning {
 		table = new HashMap<QNode, Double>();
 	}
 
+	/**
+     * This method is the main part of learning process
+     */
 	void calculateQ() {
 		Random rand = new Random();
 		for (int i = 0; i < trainingEpoch; i++) { // Train cycles
@@ -108,6 +182,11 @@ public class QLearning {
 		System.out.println("Size of table:" + table.size());
 	}
 
+	/**
+     * This method is used to do calculate the maximum Q value got the given state
+     * @param  nextState the input state
+     * @return the Q value of the given state
+     */
 	double maxQ(State nextState) {
 		int[] actionsFromState = nextState.possibleActionsFromState();
 		double maxValue = -Double.MAX_VALUE;
@@ -122,6 +201,11 @@ public class QLearning {
 		return maxValue;
 	}
 
+	/**
+     * This method is used to do calculate the maximum Q with exploration function implemented
+     * @param  nextState the input state
+     * @return the Q value of the given state
+     */
 	double modified_maxQ(State nextState) {
 		int[] actionsFromState = nextState.possibleActionsFromState();
 		double maxValue = -Double.MAX_VALUE;
@@ -136,6 +220,11 @@ public class QLearning {
 		return maxValue;
 	}
 
+	/**
+     * This method is used to extract policy from the given state
+     * @param  state the input state
+     * @return the policy of the given state
+     */
 	int getPolicyFromState(State state) {
 		int[] actionsFromState = state.possibleActionsFromState();
 		double maxValue = -Double.MAX_VALUE;
@@ -153,6 +242,10 @@ public class QLearning {
 		return action;
 	}
 
+	/**
+     * This method is used to extract policy from the Q table
+     * @return the policy extracted from Q table
+     */
 	String getStrategyFromTable() {
 		String result = "";
 		State test = new State(initState);
@@ -183,6 +276,13 @@ public class QLearning {
 		return result;
 	}
 
+	/**
+     * This method is used to decide whether to adopt a random action or based on policy
+     * @param numOfEpoches the number of current epochs
+     * @param decay the decay parameter for randomization
+     * @param k the parameter for epsilon greedy
+     * @return the result is true if it decides to move randomly and the result is false if it decides to move based on policy
+     */
 	boolean epsilonGreedy(int numOfEpoches, double decay, int k) {
 		Random rand = new Random();
 		int n = rand.nextInt(100) + 1;
@@ -193,6 +293,9 @@ public class QLearning {
 		}
 	}
 
+	/**
+     * This method is used to print the policy
+     */
 	void printStrategy() {
 		System.out.println("Moving strategy:");
 		String result = getStrategyFromTable();
